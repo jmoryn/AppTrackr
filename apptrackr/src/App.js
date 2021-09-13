ce2286a6a561cb889290c5210a81c488b24b753d
Jesse Moryn <macuser@Macs-MacBook-Pro.local>
2021-09-13 15:22:26 -0500

    initialize firebase, add components folder

--- apptrackr/src/App.js
+++ apptrackr/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+// import { initializeApp } from "firebase/app";
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>AppTrackr</h1>
     </div>
   );
 }
 
 export default App;
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDzhIJSMtMtccgptrpqdyPhhSAYwk_Cxas",
+//   authDomain: "apptrackr.firebaseapp.com",
+//   projectId: "apptrackr",
+//   storageBucket: "apptrackr.appspot.com",
+//   messagingSenderId: "1082212772940",
+//   appId: "1:1082212772940:web:104ec9b298b264239c58e9"
+// };
+//
+// const app = initializeApp(firebaseConfig);
+//
+// const auth = getAuth();
+//     createUserWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+// // Signed in
+// const user = userCredential.user;
+// // ...
+// })
+// .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+// // ..
+// });
